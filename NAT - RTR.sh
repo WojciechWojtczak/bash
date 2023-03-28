1c840f64e52500756f08518dd842f4996ad320b6
Wojciech Wojtczak <74097324+WojciechWojtczak@users.noreply.github.com>
2023-03-28 10:35:56 +0200

    Update NAT - RTR.sh

--- NAT - RTR.sh
+++ NAT - RTR.sh
@@ -29,3 +29,18 @@ iptables -A FORWARD -s 192.168.228.0/24 -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp -d 192.168.76.223 --dport 1001 -j DNAT --to-destination 192.168.10.101
 iptables -t nat -A PREROUTING -p tcp -d 192.168.76.223 --dport 1002 -j DNAT --to-destination 192.168.10.102
 iptables -t nat -A PREROUTING -p tcp -d 192.168.76.223 --dport 1003 -j DNAT --to-destination 192.168.10.103
+
+#Dodatkowe zabezpieczenia 
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -j DROP
+
+#Zapisanie ustwień
+iptables-save > /etc/iptables/rules.v4
+
+#Restart usługi
+systemctl restart netfilter-persistent
